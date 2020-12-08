88ed1b0b30ce04abc7d410c03cb615c7b97029cb
AlejandroMDR <almadoro@hotmail.com>
2020-12-08 18:43:53 -0500

    Implementación con Material UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,22 @@
+import { Grid, Typography } from '@material-ui/core';
 import React, { Component } from 'react'
 import FormularioCotizacion from './components/cotizacion/FormularioCotizacion';
 
 
 export class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <header>
-          <h1 className="row justify-content-center">Prototipo de formulario de cotizaciones</h1>
-        </header>
-        <FormularioCotizacion/>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <Typography variant="h4" align="center">Prototipo de formulario de cotizaciones</Typography>
+                <br/>
+                <Grid container justify="center">
+                    <Grid item xs={11} sm={9} lg={7}>
+                      <FormularioCotizacion/>
+                    </Grid>
+                </Grid>
+            </div>
+        )
+    }
 }
 
 export default App
